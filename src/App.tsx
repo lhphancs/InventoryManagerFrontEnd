eb5df4b3417c1f8a47deed414306e316b0c35988
Lee Phan <lhphancs@gmail.com>
2020-08-08 17:03:08 -0700

    Init2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { Home } from './Domain/Home';
+import { Home } from './pages/Home';
 
 function App() {
   return (
