1c23e35e231380c969617ae405b770eee9d234d4
Lee Phan <lhphancs@gmail.com>
2020-09-19 19:06:07 -0700

    Added textinput number format library

---
+++ src/domains/components/NumberFormat.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NumberFormat from "react-number-format";
+
+// Copied from https://material-ui.com/components/text-fields/#inputs
+export function NumberFormatCustom(props: any) {
+  const { inputRef, onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={inputRef}
+      onValueChange={(values) => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value,
+          },
+        });
+      }}
+      isNumericString
+    />
+  );
+}
